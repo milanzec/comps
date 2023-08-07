7b343f2982e437a158c8bcd0fc629501038b05bf
unknown <milanzec53@gmail.com>
2023-08-07 18:17:15 +0200

    customizing components

--- src/app/elements/elements-routing.module.ts
+++ src/app/elements/elements-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
 const routes: Routes = [
-  {path:'', component:ElementsHomeComponent}
+  { path: '', component: ElementsHomeComponent }
 ];
 
 @NgModule({
