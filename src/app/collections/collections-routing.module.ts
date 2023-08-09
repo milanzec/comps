c59cab50b9d2eb69f35836a64cef82abf0b08651
unknown <milanzec53@gmail.com>
2023-08-09 18:55:45 +0200

    routers

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ColectionsHomeComponent } from './colections-home/colections-home.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 const routes: Routes = [
-  { path: '', component: ColectionsHomeComponent }
+  {
+    path: '',
+    component: ColectionsHomeComponent,
+    children: [
+      { path: '', component: BiographyComponent },
+      { path: 'companies', component: CompaniesComponent },
+      { path: 'partners', component: PartnersComponent }
+    ]
+
+  }
 ];
 
 @NgModule({
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -5,12 +5,18 @@ import { CollectionsRoutingModule } from './collections-routing.module';
 import { ColectionsHomeComponent } from './colections-home/colections-home.component';
 import { TableComponent } from './table/table.component';
 import { SharedModule } from '../shared/shared.module';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 @NgModule({
   declarations: [
     ColectionsHomeComponent,
-    TableComponent
+    TableComponent,
+    BiographyComponent,
+    CompaniesComponent,
+    PartnersComponent
   ],
   imports: [
     CommonModule,
