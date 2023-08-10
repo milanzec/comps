be044b1f2dcc7480bcebbe182b6d14df95d154e0
unknown <milanzec53@gmail.com>
2023-08-10 23:52:44 +0200

    modal body appending

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+
+
+constructor(private element:ElementRef){
+  console.log(this.element.nativeElement)
+}
+
+ngOnInit(){
+document.body.appendChild(this.element.nativeElement)
+}
 
 }
